d77737d5d8ab0e76a0e687f6574851027124789f
Juliana Aigueira <julianaaigueira@gmail.com>
2026-03-31 17:48:47 -0300

    Configure PostgreSQL database with Neon

--- web_tradutor/settings.py
+++ web_tradutor/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,11 +73,12 @@ WSGI_APPLICATION = 'web_tradutor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# O Django vai tentar ler o banco da nuvem. Se não achar (como no seu computador local), ele usa o SQLite padrão.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
